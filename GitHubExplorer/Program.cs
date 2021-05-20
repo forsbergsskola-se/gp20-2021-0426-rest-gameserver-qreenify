e251e11a65a1c6ceb917b82051db9b8450929cdd
qreenify <adam@tapper.one>
2021-05-20 14:06:28 +0200

    first commit GitHub Explorer

---
+++ GitHubExplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
